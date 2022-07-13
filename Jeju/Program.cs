c82ccb3882937ea2a5586fba95d3193b689d79a4
dayo <dayo05@daum.net>
2022-07-13 23:50:58 +0900

    rafswrffewefw

--- Jeju/Program.cs
+++ Jeju/Program.cs
@@ -59,7 +59,7 @@ class JejuDataset : Dataset
 
     public override Dictionary<string, Tensor> GetTensor(long index) => new Dictionary<string, Tensor>
     {
-        { "label", tensor(int.Parse(files[(int)index].Split(Path.DirectorySeparatorChar)[^2]), ScalarType.Float32) },
+        { "label", tensor(int.Parse(files[(int)index].Split(Path.DirectorySeparatorChar)[^2])) },
         { "image", ReadImage(files[(int)index]) }
     };
     
